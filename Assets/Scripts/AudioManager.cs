1cff14c86f11ed8a3b5a66f7748549e9f325ff9f
Eric Lin <elin03277@gmail.com>
2020-09-03 20:00:07 -0700

    Revert "Revert "Inital Commit, Assets, Enemy/Player Control, Main Menu, Settings, Sound, Data Storage""
    This reverts commit 032d0dc9f294306a08338f3f7c40644459031be8.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    // Singleton variable to make sure there is only one audio manager
+    public static AudioManager instance;
+
+    // Use this for initialization
+     void Awake()
+    {
+        // Checks if audio manager is already created and creates one if it's not
+        // Prevents creating another manager
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+    }
+
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound: " + name + " not found");
+            return;
+        }
+
+        s.source.Play();
+    }
+}
