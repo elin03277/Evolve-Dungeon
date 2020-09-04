8a35a51c8c3e6607bae7a89bcbaf7cb0bbf61044
Eric Lin <elin03277@gmail.com>
2020-09-03 19:55:05 -0700

    Revert "Revert "Inital Commit, Assets, Enemy/Player Control, Main Menu, Settings, Sound, Data Storage""
    This reverts commit 5718bf327716a2b2026ca1bcb75f48103bc70991.

---
+++ Assets/Scripts/SaveSystem.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    private static readonly string SAVE_FOLDER = Application.dataPath + "/SaveFolder";
+
+    public static void Init()
+    {
+        if (!Directory.Exists(SAVE_FOLDER)) {
+            Directory.CreateDirectory(SAVE_FOLDER);
+        }
+    }
+
+    public static void Save(string saveString)
+    {
+        int saveNum = 1;
+
+        while (File.Exists("save_" + saveNum + ".txt"))
+        {
+            saveNum++;
+        }
+
+        File.WriteAllText(SAVE_FOLDER + "/save_" + saveNum + ".txt", saveString);
+    }
+
+    public static string Load()
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(SAVE_FOLDER);
+        FileInfo[] saveFiles = directoryInfo.GetFiles();
+        FileInfo mostRecentFile = null;
+
+        foreach (FileInfo fileInfo in saveFiles)
+        {
+            if (mostRecentFile == null)
+            {
+                mostRecentFile = fileInfo;
+            } else if (fileInfo.LastWriteTime > mostRecentFile.LastWriteTime) {
+                mostRecentFile = fileInfo;
+            }
+        }
+
+        if (mostRecentFile != null)
+        {
+            string save = File.ReadAllText(mostRecentFile.FullName);
+
+            return save;
+        } else {
+            return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject projectilePrefab;
+    public Camera cam;
+
+    public float projectileForce = 20f;
+
+    Vector3 mousePos;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // Reference and inspector if need to play more
+            FindObjectOfType<AudioManager>().Play("SwordFire");
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+        Vector3 rotationZ = mousePos - firePoint.position;
+        float angle = Mathf.Atan2(rotationZ.y, rotationZ.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;  //Quaternion.Euler(0f, 0f, angle);
+        rb.AddForce(rotationZ * projectileForce, ForceMode2D.Impulse);
+    }
+}
